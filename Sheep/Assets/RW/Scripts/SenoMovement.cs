c0503cce098a51d04813beb5242baeab8a4bde8c
AlexRunin <86658413+SciFlyy@users.noreply.github.com>
2021-06-30 21:10:22 +0300

    ADD_CLASS_WORK_30_06_21
    ADD_CLASS_WORK_30_06_21

---
+++ Sheep/Assets/RW/Scripts/SenoMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenoMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 direction;
+
+    
+
+    private void FixedUpdate()
+    {
+        transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+}
--- Sheep/Assets/RW/Scripts/TraktorMovement.cs
+++ Sheep/Assets/RW/Scripts/TraktorMovement.cs
@@ -2,55 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum TractorState { Move, Stop };
+
+
 public class TraktorMovement : MonoBehaviour
 {
 
     [SerializeField] private float speed;
-    private float direction;
+    [SerializeField] private GameObject seno;
+    [SerializeField] private float fireRate; 
+    private float nextFire;
+    private float unnextFire;
 
-    private bool isMove;
-   
-    void Start()
+    [SerializeField] private Transform senoManager;
+    private Transform spawnPoint;
+    
+    //private bool isMove;
+    private float direction;
+    private TractorState tractorState = TractorState.Stop;
+
+
+    private void Awake()
     {
-        
+        spawnPoint = transform.GetChild(1);
     }
 
 
     void Update()
     {
-        // Двигаем наш объект
-        //if ((transform.position.x <= 22) && (direction == 1f))
-        //{
-        //    transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
-        //}
-        //else if ((transform.position.x >= -22) && (direction == -1f))
-        //{
-        //    transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
-        //}
 
-        if (isMove == true) 
+        MoveTractor();
+        nextFire -= Time.deltaTime;
+    }
+
+    private void MoveTractor()
+    {
+        if (tractorState == TractorState.Move)
         {
             if (((transform.position.x <= 22) && (direction == 1f)) || ((transform.position.x >= -22) && (direction == -1f)))
-            { 
+            {
                 transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
-            }          
-        } 
+            }
+        }
     }
 
     public void MoveRight() //обработка событий
     {
         direction = 1f;
-        isMove = true;
+        //isMove = true;
+        tractorState = TractorState.Move;
     }
     public void MoveLeft()
     {
         direction = -1f;
-        isMove = true;
+        //isMove = true;
+        tractorState = TractorState.Move;
     }
     public void StopeMove()
     {
         //direction = 0f;
-        isMove = false;
+        //isMove = false;
+        tractorState = TractorState.Stop;
+    }
+
+
+
+    // Делаем кнопку FIRE
+    //public void Fire(string message)
+    //{
+    //    //Debug.Log("Fire");
+    //    Debug.Log(message);
+    //}
+    public void Fire()
+    {
+        if (nextFire < 0)
+        {
+            GameObject seno = Instantiate(this.seno, spawnPoint.position, this.seno.transform.rotation); // Объект создался
+            seno.transform.SetParent(senoManager);
+            Destroy(seno, 10f);
+            nextFire = fireRate;
+        }
+
+        //else if (nextFire > 1)
+        //{
+        //    GameObject seno = Instantiate(this.seno, spawnPoint.position, this.seno.transform.rotation); // Объект создался
+        //    seno.transform.SetParent(senoManager);
+        //    Destroy(seno, 10f);
+        //    nextFire = fireRate;
+        //}
     }
 
 }
