17a22bceb07318919d26d34c0d45f34198d76c8d
AlexRunin <86658413+SciFlyy@users.noreply.github.com>
2021-07-07 21:03:19 +0300

    Sheep_death_heart_effect_07_07_21
    Sheep_death_heart_effect_07_07_21

--- Sheep/Assets/RW/Scripts/SenoMovement.cs
+++ Sheep/Assets/RW/Scripts/SenoMovement.cs
@@ -6,11 +6,32 @@ public class SenoMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Vector3 direction;
+    [SerializeField] private float jumpForce;
+    private Rigidbody rb;
+    [SerializeField] private GameObject heartEffect;
 
-    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void FixedUpdate()
     {
         transform.Translate(direction * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) // other -обект с которым столкнулись
+    {
+        if (other.gameObject.tag == "Seno")
+        {
+            rb.isKinematic = false;
+            GetComponent<BoxCollider>().enabled = false;
+            rb.AddForce(Vector3.up * jumpForce); // прыгаем
+            Destroy(gameObject, 1f); //уничтажаем сено Sheep
+            Destroy(other.gameObject); //уничтажаем сено 
+
+            GameObject effect = Instantiate(heartEffect, transform.position + new Vector3(0f, 5f, 0f), heartEffect.transform.rotation); // Spawn
+            Destroy(effect, 1f);
+        }
     }
 }
